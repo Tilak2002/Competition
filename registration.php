16312d82b313d08a3a2e2ea1de9db665cf3912f9
Tilak2002 <swamitilak@gmail.com>
2022-07-07 10:29:49 +0530

    til

--- registration.php
+++ registration.php
@@ -110,6 +110,55 @@ include 'class.php';
             let bio=$('#bio').val();
 
 
+    var valid = check();
+
+        if(!valid){
+            return;
+        }
+
+        function check()
+        {
+            if(stuname==""){
+                frm.ocname.focus();
+                alert("Please fill the 'Student Name' field");
+                return false;
+            }
+
+            else if(checkSTname(stuname))
+                {
+                    alert("Only characters are allowed in the 'Student Name' field");
+                    frm.ocname.focus();
+                    return false;
+                }
+
+            if(phy==""){
+                frm.phy.focus();
+                alert("Please fill the 'Physics(Marks)' field");
+                return false;
+            }
+
+            if(che==""){
+                frm.che.focus();
+                alert("Please fill the 'Chemistry(Marks)' field");
+                return false;
+            }
+
+            if(maths && bio ==""){
+                frm.bio.focus();
+                alert("Please fill both the 'Maths(Marks) and Biology(Marks)' field");
+                return false;
+            }
+
+                else{
+                    return true;
+                }
+        }
+
+            function checkSTname(stuname)
+            {
+                return /[\W\d]+$/.test(stuname);
+            }
+
             console.log(loopInc);
              var activity = [];
              
@@ -119,167 +168,18 @@ include 'class.php';
                 //  qua[i-1]=$('#quantity_'+i).val();
                  
                 }
-            // let sadd=$('#sadd').val();
-            // let sstate=$('#sstate').val();
-            // let scode=$('#scode').val();
-
-            // let gstin=$('#gstin').val();
-            // let length = gstin.length;
-            // console.log(length);
-
-
-
-        // var valid = check();
-        // if(!valid){
-        //     return;
-        // }
-
-        // function check()
-        // {
-        //     if(ocname==""){
-        //         frm.ocname.focus();
-        //         alert("Please fill the 'Order Customer Name' field");
-        //         return false;
-        //     }
-
-        //     else if(checkOCname(ocname))
-        //         {
-        //             alert("Only characters are allowed in the 'Order Customer Name' field");
-        //             frm.ocname.focus();
-        //             return false;
-        //         }
-
-
-        //     if(gstin==""){
-        //         frm.gstin.focus();
-        //         alert("Please fill the 'GSTIN' field");
-        //         return false;
-        //     }
-
-        //     else if(length!=15){
-        //             alert("Exactly 15 characters are allowed in the 'GSTIN' field");
-        //             frm.gstin.focus();
-        //             return false;
-        //         }
-
-
-        //      if(mobno==""){
-        //          frm.mobno.focus();
-        //          alert("Please fill the 'Mobile number' field");
-        //          return false;
-        //     }
-
-        //     else if(!checkMobNo(mobno))
-        //         {
-        //             alert("Only 10 digits (Starting from 6-9) are allowed in the 'Mobile number' field ");
-        //             frm.mobno.focus();
-        //             return false;
-        //         }
-
-        //      if(badd==""){
-        //          frm.baddress.focus();
-        //          alert("Please fill the 'Billing Address' field");
-        //          return false;
-        //     }
-
-        //     else if(checkBAdd(badd))
-        //         {
-        //             alert("Only characters and digits are allowed in the 'Billing Address' field");
-        //             frm.baddress.focus();
-        //             return false;
-        //         }
-
-        //      if(bcode==""){
-        //          frm.bcode.focus();
-        //          alert("Please fill the 'Pincode' field");
-        //          return false;
-        //     }
-
-        //     else if(!checkBCode(bcode))
-        //         {
-        //             alert("Only 6 digits are allowed in the 'Pincode' field");
-        //             frm.bcode.focus();
-        //             return false;
-        //         }
-
-        //      if(sadd==""){
-        //          frm.saddress.focus();
-        //          alert("Please fill the 'Shipping Address' field");
-        //          return false;
-        //     }
-
-        //     else if(checkSAdd(sadd))
-        //         {
-        //             alert("Only characters and digits are allowed in the 'Shipping Address' field");
-        //             frm.saddress.focus();
-        //             return false;
-        //         }
-
-        //         if(scode==""){
-        //          frm.scode.focus();
-        //          alert("Please fill the 'Pincode' field");
-        //          return false;
-        //     }
-
-        //     else if(!checkSCode(scode))
-        //         {
-        //             alert("Only 6 digits are allowed in the 'Pincode' field");
-        //             frm.scode.focus();
-        //             return false;
-        //         }
-
-        //     else{
-        //         return true;
-        //     }
-        // }
-
-       
-        // function checkOCname(ocname)
-        //     {
-        //         return /[\W\d]+/.test(ocname);
-        //     }
-
-        // // function checkGSTIN(gstin)
-        // //     {
-        // //         return /[\W\D]{15}$/.test(gstin);
-        // //     }
-
-        // function checkMobNo(mobno)
-        //     {
-        //         return /^[6-9]{1}[\d]{9}$/.test(mobno);
-        //     }
-
-        // function checkBAdd(badd)
-        //     {
-        //         return /^[\W\D]$/.test(badd);
-        //     }
-
-        // function checkBCode(bcode)
-        //     {
-        //         return /^[\d]{6}$/.test(bcode);
-        //     }
-
-        //     function checkSAdd(sadd)
-        //     {
-        //         return /^[\W\D]$/.test(sadd);
-        //     }
-
-        //     function checkSCode(scode)
-        //     {
-        //         return /^[\d]{6}$/.test(scode);
-        //     }
 
 
             $.ajax({
                 method:"post",
                 url:"http://localhost/Competition/inst_registration.php",
                 data: {
-                    stuname  :stuname,
-                    phy  :phy,
-                    che   :che,
-                    mat    :mat,
-                    bio  :bio,
-                    activity: activity
+                    stuname :stuname,
+                    phy:phy,
+                    che:che,
+                    mat:maths,
+                    bio:bio,
+                    activity:activity
                     
                 }
             }).done(function(msg){
